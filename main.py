3887b3ea908a3750b26eb1713b0008c0178aabc9
DarkArtheme <artyom.bogdanov.2012@yandex.ru>
2021-07-13 23:20:46 +0300

    Added .gitignore and useless comments in main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from db_manager import DBManager
 from io_manager import IOManager
 
 
+# this is main
 def main():
     db = DBManager()
     IOManager.exec_cmd(["ls", "-l", "."])
@@ -10,3 +11,5 @@ def main():
 if __name__ == "__main__":
     main()
 
+# random comment
+# another line
